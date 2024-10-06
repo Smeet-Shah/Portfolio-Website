e95174dbe9ea4ce9bf737f3f0ad7656fb6dd3549
Smeet Shah <69440975+Smeet-Shah@users.noreply.github.com>
2024-10-05 20:48:35 -0400

    Removed api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -12,19 +12,3 @@ export const getReadme = async () => {
   const { data } = await axios.get(config.readmeUrl);
   return data;
 };
-
-export const getWeather = async (city: string) => {
-  try {
-    const { data } = await axios.get(`https://wttr.in/${city}?ATm`);
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
-
-export const getQuote = async () => {
-  const { data } = await axios.get('https://api.quotable.io/random');
-  return {
-    quote: `“${data.content}” — ${data.author}`,
-  };
-};
