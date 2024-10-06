11b820f790e3785fb47c09eae73f840c4921d4a4
Smeet Shah <69440975+Smeet-Shah@users.noreply.github.com>
2024-10-05 20:49:01 -0400

    Update api_commands.ts

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -15,22 +15,9 @@ export const projects = async (args: string[]): Promise<string> => {
     .join('\n');
 };
 
-export const quote = async (args: string[]): Promise<string> => {
-  const data = await getQuote();
-  return data.quote;
-};
-
 export const readme = async (args: string[]): Promise<string> => {
   const readme = await getReadme();
   return `Opening GitHub README...\n
   ${readme}`;
 };
 
-export const weather = async (args: string[]): Promise<string> => {
-  const city = args.join('+');
-  if (!city) {
-    return 'Usage: weather [city]. Example: weather casablanca';
-  }
-  const weather = await getWeather(city);
-  return weather;
-};
