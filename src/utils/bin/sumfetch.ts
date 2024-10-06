c8b8e27606e956041ee83392b8aeff466587720a
smeet <s56shah@uwaterloo.ca>
2024-10-05 20:39:50 -0400

    first commit

---
+++ src/utils/bin/sumfetch.ts
@@ -0,0 +1,24 @@
+import config from '../../../config.json';
+
+const sumfetch = async (args: string[]): Promise<string> => {
+    return `
+           ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄                  sumfetch
+        ▄▓▓▀ ▄▓▓▀▓▓▓▀▓▓▄ ▀▀▓▓▄              -----------
+      ▓▓▀  ▄▓▀   ▐▓▓  ▀▓▓    ▓▓▄             ABOUT
+    ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            ${config.name}
+   ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓           <u><a href="${config.resume_url}" target="_blank">resume</a></u>
+▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
+▐▓                                 ▐▓       -----------
+▐▓      S M E E T'S  S I T E       ▐▓        CONTACT 
+▐▓                                 ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓        <u><a href="${config.social.github}" target="_blank">${config.social.github}</a></u>
+   ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓           <u><a href="${config.social.linkedin}" target="_blank">${config.social.linkedin}</a></u>
+    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓           -----------
+      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀             CURRENT SONG
+        ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀                <u><a href="https://www.youtube.com/watch?v=Kk8eJh4i8Lo&pp=ygULYnVsbGV0cHJvb2Y%3D" target="_blank">www.youtube.com/watch?v=Kk8eJh4i8Lo&pp=ygULYnVsbGV0cHJvb2Y%3D</a></u>
+            ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                  
+
+`;
+};
+
+export default sumfetch;
